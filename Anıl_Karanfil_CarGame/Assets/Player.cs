68fef3df25a1bce2250e4f51fec00a6e14ac45b3
anylk <karanfilanil@outlook.com>
2020-09-13 22:40:18 +0300

    CardGame

---
+++ Anıl_Karanfil_CarGame/Assets/Player.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    public Rigidbody rb;
+
+    public float speed;
+    Vector3 velocity;
+    private Vector3 inputVector;
+
+    bool isPressedLeft;
+    bool isPressedRight;
+
+    Recorder recorder;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        recorder = GetComponent<Recorder>();
+        recorder.startingPos = transform.position;
+    }
+
+    // Update is called once per frame
+    public float timer;
+    void Update()
+    {
+
+        velocity = Vector3.zero;
+
+        Vector2 mouse = new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y);
+
+
+
+
+        if (Input.GetMouseButton(0))//Tab the screen
+        {
+
+            if (mouse.x < Screen.width / 2)//Left side of screen.
+            {
+                isPressedLeft = true;
+            }
+
+            if (mouse.x > Screen.width / 2)//Right side of screen.
+            {
+                isPressedRight = true;
+            }
+        }
+
+
+        if (Input.GetMouseButtonUp(0))//Stop touch of screen.
+        {
+            if (isPressedLeft == true)
+            {
+                recorder.recordDataList.Add(new RecordData(-1, timer, timerPress));
+
+            }
+            if (isPressedRight == true)
+            {
+                recorder.recordDataList.Add(new RecordData(1, timer, timerPress));
+
+            }
+            timer = 0;
+            timerPress = 0;
+            isPressedRight = false;
+            isPressedLeft = false;
+        }
+
+        
+
+
+    }
+
+    float timerPress;
+    private void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+
+        if (isPressedLeft)
+        {
+            timerPress += Time.deltaTime;
+            MoveHorizontal(-1);
+        }
+
+
+        if (isPressedRight)
+        {
+            timerPress += Time.deltaTime;
+            MoveHorizontal(1);
+
+        }
+        MoveVertical();//Car always moves in a positive direction
+    }
+
+    public void MoveVertical()//Car always moves in a positive direction
+    {
+        velocity.z = 10;
+        transform.Translate(velocity.normalized * Time.deltaTime * speed);
+    }
+    public void MoveHorizontal(int charge)//Car moves in a positive or negative direction
+    {
+        velocity.x = charge;
+        transform.Rotate(0, charge * 45 * Time.deltaTime, 0);
+    }
+
+
+}
---
+++ Anıl_Karanfil_CarGame/Assets/Recorder.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recorder : MonoBehaviour
+{
+
+    public List<RecordData> recordDataList = new List<RecordData>();
+
+    public Vector3 startingPos;
+    public bool isRecorded;
+    Player player;
+
+    //Compare with recorded values 
+    public float playingTime;
+    public int counter;
+    void Start()
+    {
+        player = GetComponent<Player>();
+        counter = 0;
+        transform.position = StartPos();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        playingTime += Time.deltaTime;
+
+        if (isRecorded)//We have Records?
+        {
+            if(counter != recordDataList.Count)
+            {
+
+
+                if (recordDataList[counter].recordTime == playingTime)//Is Record Time and Playing Time is Equal
+                {
+                    Debug.Log("-1");
+
+                    StartCoroutine(PressKey(recordDataList[counter].turnDirect, recordDataList[counter].recordLength));
+                    counter++;
+
+                }
+            }
+            else
+            {
+                Debug.Log("done");
+            }
+            player.MoveVertical();
+
+        }
+
+    }
+
+
+    IEnumerator PressKey(int keyCode, float Ptime)
+    {
+
+        while  (Ptime >= 0f)
+        {
+            Debug.Log("  aaa  "+Time.deltaTime);
+
+            if (keyCode == 1)//Did you press the right side
+            {
+                
+                player.MoveHorizontal(1);
+            }
+            if(keyCode == -1)//Did you press the left side
+            {
+                player.MoveHorizontal(-1);
+
+            }
+            Ptime -= Time.deltaTime;
+
+
+        }
+
+
+        yield return new WaitForSeconds(Ptime);
+
+    }
+
+    public Vector3 StartPos()
+    {
+        return startingPos;
+    }
+}
